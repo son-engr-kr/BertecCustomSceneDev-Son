3c42d6742929e4c79fcc674f7f7528153d2aaf86
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-14 20:20:28 -0400

    Custom Scene test

--- Assets/Scenes/VisualFlow/VisualFlow.cs
+++ Assets/Scenes/VisualFlow/VisualFlow.cs
@@ -682,6 +682,7 @@ public class VisualFlow : MonoBehaviour
         // move the blocks first then reposition
         foreach (GroundBlock block in GroundBlocks)
         {
+            Debug.Log($"{block.name}");
             block.transform.position += Vector3.back * (Time.deltaTime * VisualFlowSpeed * FlowSpeedScaler);
         }
 
